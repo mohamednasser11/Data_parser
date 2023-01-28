c38be35df88fd7e7aad0e130039fff83acdb13fe
mohamednasser11 <mohamednaser1000.mn@gmail.com>
2023-01-28 04:34:08 +0200

    adding timestamp with the name in the output

--- parsers/abstract_Handler.py
+++ parsers/abstract_Handler.py
@@ -1,7 +1,7 @@
 from abc import abstractmethod
 from parsers.reading_files_interface import ReadingFilesInterface
 from typing import Optional
-import random as rand
+from datetime import datetime
 from pathlib import Path
 import json
 class AbstractHandler(ReadingFilesInterface):
@@ -15,27 +15,20 @@ class AbstractHandler(ReadingFilesInterface):
     
     @abstractmethod
     def parsing(self, file) -> Optional[str]:
-        
         if self._next_parser:
             return self._next_parser.parsing(file)
         else:
             return None
     
     
-    def export_to_json(self, data,destination:str):
-        randname = self.random_filename()
-        filename = randname
+    def export_to_json(self,filename:str, data, destination:str):
+        ts = str(datetime.timestamp(datetime.now()))
+        filename = ts + filename +'.json'
+        
         filepath = Path(r"./output/"+destination+filename)
         filepath.parent.mkdir(parents=True,exist_ok=True)
         with open(filepath, 'w') as file:
             json.dump(data,file, indent=4)
         print('Exporting Done')
 
-    def random_filename(self):
-        filename = "file.json"
-        enc = ''
-        for i in range(0,15):
-            num = rand.randint(0,10)
-            enc += str(num)
-        filename = enc + filename
-        return filename
+   
--- parsers/xml_parsing.py
+++ parsers/xml_parsing.py
@@ -1,14 +1,15 @@
 from parsers.abstract_Handler import AbstractHandler
 import xmltodict
-
+from datetime import datetime
 
 class XmlParsing(AbstractHandler):
     def parsing(self, file:str) -> str:
+        name = file.split('.')[0]
         if file.endswith('.xml'):
             with open(file, 'r', encoding='utf-8') as xmlFile:
                 myXml = xmlFile.read()
             data = xmltodict.parse(myXml)
-            return self.export_to_json(data,'xml/')
+            return self.export_to_json(data=data,destination='xml/', filename=name)
         else:
             return super().parsing(file)
 
